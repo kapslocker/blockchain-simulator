c6c741b4ee184be99865d535dea44d21c2a4314c
Kapil Ahuja <kapil99a@gmail.com>
2017-02-17 22:18:01 +0530

    Events template

--- Event.java
+++ Event.java
@@ -1,8 +1,22 @@
 public class Event{
-    char type;
+    int type;
     double scTime;
 
     void execute(Simulator s){
-        
+        switch(type){
+            case 0:
+                // Block generate
+                break;
+            case 1:
+                // Transaction generate
+                break;
+            case 2:
+                // Block receive
+                break;
+            case 3:
+                // Transaction receive
+                break;
+
+        }
     }
 }
